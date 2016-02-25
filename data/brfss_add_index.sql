beffa40cc66598512a1a54a0d66ee7ef87a998a8
Brian High <bhigh@live.com>
2016-02-25 10:46:11 -0800

    Refined SQL query, removed redundant filtering, added indexes.

--- data/brfss_add_index.sql
+++ data/brfss_add_index.sql
@@ -1,3 +1,5 @@
 ALTER TABLE brfss ADD INDEX iyear_ndx (IYEAR);
 ALTER TABLE brfss ADD INDEX iyear_x_state_ndx (IYEAR, X_STATE);
+ALTER TABLE brfss ADD INDEX iyear_x_state_x_age80_ndx (IYEAR, X_STATE, X_AGE80);
+ALTER TABLE brfss ADD INDEX iyear_x_age80_ndx (IYEAR, X_AGE80);
 ALTER TABLE brfss ADD INDEX state_ndx (X_STATE);
